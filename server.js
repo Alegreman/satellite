80e46b88efc4645c0da51987c421953396aafe22
alegreman <alegreman09@gmail.com>
2019-10-10 10:43:13 -0600

    joa

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/satellite'));
+app.use(express.static(__dirname + '/dist/cigna-project'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/satellite/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cigna-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
