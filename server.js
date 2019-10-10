682f0326055dd8d602686798e871761017ba16e5
alegreman <alegreman09@gmail.com>
2019-10-10 10:24:02 -0600

    jojdiofjsd

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/cigna-project'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/app.component.html'));
+  res.sendFile(path.join(__dirname + '/dist/cigna-project/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
